2cb36306c095672722e7744e22b86978c2d2bef7
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-02-11 15:15:54 +0300

    Cleaned F90 wrapper from junk code

--- expm/explib.f90
+++ expm/explib.f90
@@ -10,9 +10,6 @@ module explib
      integer :: lwsp 
      integer :: liwsp 
      double precision, allocatable :: wsp(:)
-     !TMP SECTION
-     double precision, allocatable :: tmp(:,:),vtmp(:)
-     integer :: k
      integer, allocatable :: iwsp(:)
      integer :: itrace = 0
      integer iflag
@@ -47,14 +44,10 @@ module explib
      integer :: ideg = 6
      integer :: lwsp 
      integer :: liwsp 
-     complex(8), allocatable :: wsp(:)
-     !TMP SECTION
-     integer :: k
      integer, allocatable :: iwsp(:)
      integer :: itrace = 0
      integer iflag
-     !Test area
-     integer i,j, iv, j1v, mh, ih
+     complex(8), allocatable :: wsp(:)
      external matvec
      lwsp = (n*(m+1)+n+(m+2)**2+4*(m+2)**2+ideg+1)
      lwsp = max(lwsp,200)
@@ -66,25 +59,7 @@ module explib
         call matvec((1d0,0d0),w)
         w(1) = v(1) * exp(w(1) * tau)
      else
-     !   call zcopy( n, v,1, w,1 )
-     !   mh = m1 + 2
-     !   iv = 1
-     !   j1v = 5
-     !   ih = iv + n*(m+1) + n
-     !   do i = 1,n
-     !      wsp(iv + i-1) = w(i)
-     !   enddo
-     !   do i = 1,mh*mh
-     !      wsp(ih+i-1) = ZERO
-     !   enddo
-        !call matvec( wsp(j1v-n), wsp(j1v) )
-        !call zcopy(n,v,1,w,1)
-        !call matvec(v,w)
-        !print *,'old core:', v(1:2)
         call zgexpv(n,m1,tau,v,w,tol,anorm,wsp,lwsp,iwsp,liwsp,matvec,itrace,iflag)
-        !print *,'new core:', w(1:2)
-        !print *,'true sol:', exp(tau)*v(1:2)
-        !read(*,*)
         if ( iflag .ne. 0 ) then
            print *,'exp_mv failed with iflag=',iflag
         end if
