66dc9c9be4604bc82e873f07e4d7abac68bec977
Qbit- <rss3@rice.edu>
2018-03-22 14:26:11 +0300

    This version is cleaned and fails on matvec

--- expm/normest.f90
+++ expm/normest.f90
@@ -1,6 +1,7 @@
 !Implements a user-friendly interface to the Higham norm estimator
 module estnorm
-integer :: iseed(4)
+  use dispmodule
+  integer :: iseed(4)
 contains 
 
   subroutine init_seed()
@@ -24,7 +25,6 @@ contains
     t = min(n,t0)
     x(:,:) = 0d0
     
-    
     do while ( (kase .ne. 0) .or. (i .eq. 1) ) 
        call dlacn1(n,t,v,x,n,xold,n,wrk,H,ind,indh, est, kase, iseed, info)
        !if ( (kase .eq. 0) .and. ((info .ne. 2) .and. (info .ne. 3))) then
@@ -32,22 +32,22 @@ contains
        !end if
        if ( kase .eq.  1 ) then
           do k = 1,t
-            
-          !call matvec(x(:,k),xold(:,k))
-          end do 
+             !call matvec(x(:,k),xold(:,k))
+          end do
           !call dcopy(n*t,xold,1,x,1)
        else if ( kase .eq. 2 ) then
           do k = 1,t
-            !call matvec(x(:,k),xold(:,k))
-            !call dcopy(n*t,xold,1,x,1)
-          end do 
+             !call matvec(x(:,k),xold(:,k))
+             !call dcopy(n*t,xold,1,x,1)
+          end do
           !call dcopy(n*t,xold,1,x,1)
        else if ( kase .ne. 0 ) then
           print *,'norm est failed with kase=',kase 
        end if
        i = i + 1
-    end do 
+    end do
   end function normest
+
   function znormest(n,t0, matvec, matvec_transp) result(est)
     implicit none
     integer, intent(in) :: n, t0
@@ -57,8 +57,8 @@ contains
     complex(8) ::  ZERO, ONE
     parameter( ZERO=(0.0d0,0.0d0), ONE=(1.0d0,0.0d0) )
 
-    real(8) :: H(n)
     integer :: ind(n), indh(n), info
+    double precision :: H(n)
     double precision :: est
     integer :: kase,i,k !The reverse communication stuff
     i = 1 
@@ -66,23 +66,24 @@ contains
     t = min(n,t0)
 
     x(:,:) = ZERO
+    xold(:,:) = ZERO
     do while ( (kase .ne. 0) .or. (i .eq. 1) ) 
-        call zlacn1(n, t, v, x, n, xold, n, H,  &
-            ind, indh, est, kase, iseed, info)
+        call zlacn1(n, t, v, x, n, xold, n, H, ind, indh, est, kase, iseed, info)
         !if ( (kase .eq. 0) .and. ((info .ne. 2) .and. (info .ne. 3))) then
         !   print *,'normest failed with info=',info
         !end if
         if ( kase .eq.  1 ) then
-            do k = 1,t
-                call matvec(x(:, k), xold(:, k))
-            end do 
-            call zcopy(n * t, xold, 1, x, 1)
+           do k = 1,t
+              print *,'k=',k 
+              call matvec(x(:, k), xold(:, k))
+           end do
+           call zcopy(n * t, xold, 1, x, 1)
         else if ( kase .eq. 2 ) then
-            do k = 1,t
-                !call matvec(x(:, k), xold(:, k))
-                call matvec_transp(x(:,k), xold(:,k))
-            end do 
-                call zcopy(n * t, xold, 1, x, 1)
+           do k = 1,t
+              !call matvec(x(:, k), xold(:, k))
+              call matvec_transp(x(:,k), xold(:,k))
+           end do
+           call zcopy(n * t, xold, 1, x, 1)
         else if ( kase .ne. 0 ) then
             print *,'norm est failed with kase=',kase 
         end if
